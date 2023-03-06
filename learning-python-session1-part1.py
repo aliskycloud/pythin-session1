9b2489c50f0f37e7f173ba8d67d3263326d34fe4
ali.moayer5879 <ali.moayer5879@gmail.com>
2023-03-06 18:14:49 +0300

    Compare Minutes(now vs odd list) with while & if

--- learning-python-session1-part1.py
+++ learning-python-session1-part1.py
@@ -6,5 +6,13 @@ import time
 print('Time :',time.strftime('%H:%M:%S'))
 
 odd=[1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59]
+next='y'
+while next=='y':
+    cm = datetime.datetime.today().minute
+    if cm in odd:
+        print('Minute now is odd = ',cm)
+    else:
+        print('Minute now is not odd ')
+    next=input('Are You Continue? (y/n)')
 
 
